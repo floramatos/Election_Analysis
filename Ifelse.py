060130d3eea52be01c3adbbae6f6e91dd8d832a8
Flora Matos <floramatos@floras-mbp.lan>
2021-11-06 11:37:05 -0700

    Loaded csv file and printed rows

--- Ifelse.py
+++ Ifelse.py
@@ -9,3 +9,11 @@ else:
 
     else:
         print('test')
+
+import datetime
+now = datetime.datetime.now()
+print("The time right now is ", now)
+
+today = datetime.date.today()
+print("Today is ", today)
+dir(datetime)
--- PyPoll.py
+++ PyPoll.py
@@ -1,15 +1,26 @@
-#Find total number of votes cast
-    #Equal to number of rows = 369712? Are there blank or null votes?
+import csv
+import os
 
-#Put together a complete list of candidates who received votes
-    #Loop over rows and column 3 to find unique value for candidate
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#Find total number of votes each candidate received
-    #Loop over rows to count the number of times a candidate name appears
-    for i == 369712
-        print()
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    #To do: read and analyze the data here.
+    file_reader = csv.reader(election_data)
 
-#Calculate the percentage of votes each candidate won
-    #total number of votes for a candidate/total number of votes * 100
+    #Print the header row.
+    headers = next(file_reader)
+    print(headers)
 
-#Identify the winner of the election based on popular vote
+    #Print each row in the CSV file.
+    #for row in file_reader:
+        #print(row)
+
+# Using the with statement open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    #txt_file.write("Counties in the Election\n----------------\nArapahoe\nDenver\nJefferson")
